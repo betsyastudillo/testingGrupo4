171da2b0e91545f8e5e0a9b2a1fe2a4bbb26c112
Betsy Astudillo <betsymalelyastudillo@hotmail.com>
2024-11-26 17:24:33 -0500

    Se comentan importaciones

--- src/components/AuthUser.jsx
+++ src/components/AuthUser.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import app from '../axiosConfig'
-import { useEffect, useState } from "react";
+// import app from '../axiosConfig'sfrom "react";
 
 export function AuthUser() {
 
