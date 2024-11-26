73bc337f83313855caccec35411432e957e38ef9
Camilo Munoz <mcamiloesteban67@gmail.com>
2024-11-26 15:29:54 -0500

    view company

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from './components/NavBar';
 import { HomeView } from './views/HomeView';
 import { FormCreateCompany } from './form/FormCreateCompany';
-
+import { ReviewCompany } from "./views/ReviewCompany";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
             {/* Rutas de Contenido */}
             <Route path="/" element={HomeView()} />
             <Route path="/login" element={FormCreateCompany()} />
+            <Route path="/review-company" element={ReviewCompany()} />
           </Routes>
         </div>
       </div>
--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,15 @@ import { Routes, Route } from "react-router-dom";
 // Importa tus componentes
 import Home from "/";
 import FormCreateCompany from "./form/FormCreateCompany";
+import ReviewCompany  from "./form/ReviewCompany";
+
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/form-create-company" element={<FormCreateCompany />} />
+      <Route path="/review-company" element={<ReviewCompany />} />
     </Routes>
   );
 };
